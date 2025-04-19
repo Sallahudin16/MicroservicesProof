5afc1f011a46c3899753827b84007d614be148c8
Muhammad Salahuddin <mohamed.ayman@assetcom.eg>
2025-04-19 19:06:46 +0200

    Added Create Product Endpoint and Handler

---
+++ src/Services/Catalog/Catalog.API/Analysis.md
@@ -0,0 +1,27 @@
+# Entities
+
+- Product
+  - Many Categories
+- Category
+  - Many Products
+
+# Endpoints
+
+
+# Topics Covered
+
+- Vertical Slice Architecture
+---
+- CQRS Pattern 
+- Minimal APIs Pattern
+- ORM Pattern
+- Mediator Pattern
+---
+- Document Database
+- Postgres SQL
+---
+- Marten Library 
+- MediatR Library
+- Carter Library 
+- Mapster Libary 
+- Fluent Validation Library 
---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,16 @@
+﻿using SharedKernel.Abstractions.CQRS;
+
+namespace Catalog.API.Products.CreateProduct
+{
+    public record CreateProductCommand(string Name, List<string> Categories, string Description, string ImageFile, decimal Price) : ICommand<CreateProductResult>;
+    public record CreateProductResult(Guid Id);
+
+    internal class CreateProductHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+    {
+        public async Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
