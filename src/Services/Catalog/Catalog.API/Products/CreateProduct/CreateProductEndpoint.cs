125d0916b80b439783d124137ed2f13da2d4cfc2
Muhammad Salahuddin <mohamed.ayman@assetcom.eg>
2025-04-19 21:17:40 +0200

    Added Product Enpoint Logic

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -1,7 +1,25 @@
 ﻿namespace Catalog.API.Products.CreateProduct
 {
-    public class CreateProductEndpoint
-    {
+    public record CreateProductRequest(string Name, List<string> Categories, string Description, string ImageFile, decimal Price);
+    public record CreateProductResponse(Guid Id);
 
+    public class CreateProductEndpoint : ICarterModule
+    {
+        private const string _moduleRoute = "/products";
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPost(_moduleRoute, async (CreateProductRequest request, ISender sender) =>
+            {
+                CreateProductCommand command = request.Adapt<CreateProductCommand>();
+                CreateProductResult result = await sender.Send(command);
+                CreateProductResponse response = result.Adapt<CreateProductResponse>();
+                return Results.Created($"{_moduleRoute}/{response.Id}", response);
+            })
+            .WithName("CreateProduct")
+            .Produces<CreateProductResponse>(StatusCodes.Status201Created)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Create Product")
+            .WithDescription("Create Product Endpoint");
+        }
     }
 }
