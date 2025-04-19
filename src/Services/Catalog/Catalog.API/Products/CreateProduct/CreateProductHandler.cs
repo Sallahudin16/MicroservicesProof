061ff2491e60f15146107cee061442dd6f96a233
Muhammad Salahuddin <mohamed.ayman@assetcom.eg>
2025-04-19 21:17:54 +0200

    Added CreateProduct Logic

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,15 +1,29 @@
-﻿using SharedKernel.Abstractions.CQRS;
-
-namespace Catalog.API.Products.CreateProduct
+﻿namespace Catalog.API.Products.CreateProduct
 {
     public record CreateProductCommand(string Name, List<string> Categories, string Description, string ImageFile, decimal Price) : ICommand<CreateProductResult>;
     public record CreateProductResult(Guid Id);
 
-    internal class CreateProductHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+    internal class CreateProductHandler(IDocumentSession session) : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
         public async Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            //ccreate product entity
+            Product product = new ()
+            {
+                Id = Guid.NewGuid(),
+                Name = request.Name,
+                Categories = request.Categories,
+                Description = request.Description,
+                ImageFile = request.ImageFile,
+                Price = request.Price
+            };
+
+            //save to database
+            session.Store(product);
+            await session.SaveChangesAsync(cancellationToken);
+
+            //return result
+            return new CreateProductResult(product.Id);
         }
     }
 }
