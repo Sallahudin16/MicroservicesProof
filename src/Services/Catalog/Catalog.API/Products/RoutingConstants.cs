c8862a88e41a184c98387d983b4f8a64b41e5046
Muhammad Salahuddin <mohamed.ayman@assetcom.eg>
2025-04-20 12:30:07 +0200

    Added Routing Constants and Global Usings

---
+++ src/Services/Catalog/Catalog.API/Products/RoutingConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace Catalog.API.Products
+{
+    public class RoutingConstants
+    {
+        internal const string ModuleName = "Catalog Service";
+        internal const string ProductsRoute = "/products";
+    }
+}
