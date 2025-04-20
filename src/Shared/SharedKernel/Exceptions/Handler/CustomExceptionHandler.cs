75da90e88f8b6fc8dd87bb1f3b7d0ebe45a3b5f3
Muhammad Salahuddin <mohamed.ayman@assetcom.eg>
2025-04-20 16:00:29 +0200

    Added Validation Pipeline Behavior, Added Exception Handler and Exception Types

---
+++ src/Shared/SharedKernel/Exceptions/Handler/CustomExceptionHandler.cs
@@ -0,0 +1,73 @@
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace SharedKernel.Exceptions.Handler
+{
+    public class CustomExceptionHandler : IExceptionHandler
+    {
+        private readonly ILogger<CustomExceptionHandler> _logger;
+
+        public CustomExceptionHandler(ILogger<CustomExceptionHandler> logger)
+        {
+            _logger = logger;
+        }
+
+        public async ValueTask<bool> TryHandleAsync(HttpContext context, Exception exception, CancellationToken cancellationToken)
+        {
+            _logger.LogError(
+                "Error Message: {exceptionMessage}, Time of occurence {time}",
+                 exception.Message, DateTime.UtcNow
+            );
+
+
+            (string Detail, string Title, int StatusCode) = exception switch
+            {
+                InternalServerException =>
+                (
+                    exception.Message,
+                    exception.GetType().Name,
+                    context.Response.StatusCode = StatusCodes.Status500InternalServerError
+                ),
+                ValidationException =>
+                (
+                    exception.Message,
+                    exception.GetType().Name,
+                    context.Response.StatusCode = StatusCodes.Status400BadRequest
+                ),
+                BadRequestException =>
+                (
+                    exception.Message,
+                    exception.GetType().Name,
+                    context.Response.StatusCode = StatusCodes.Status400BadRequest
+                ),
+                NotFoundException =>
+                (
+                    exception.Message,
+                    exception.GetType().Name,
+                    context.Response.StatusCode = StatusCodes.Status404NotFound
+                ),
+                _ => ("Something Went Wrong", exception.GetType().Name, context.Response.StatusCode = StatusCodes.Status500InternalServerError)
+            };
+
+            ProblemDetails problemDetailes = new()
+            {
+                Title = Title,
+                Detail = Detail,
+                Status = StatusCode,
+            };
+
+            problemDetailes.Extensions.Add("traceId", context.TraceIdentifier);
+            if(exception is ValidationException validationException)
+            {
+                problemDetailes.Extensions.Add("ValidationErrors", validationException.Errors);
+            }
+
+            context.Response.ContentType = "application/problem+json";
+            await context.Response.WriteAsJsonAsync(problemDetailes, cancellationToken: cancellationToken);
+            return true;
+        }
+    }
+}
---
+++ src/Shared/SharedKernel/PipelineBehaviors/ValidationBehavior.cs
@@ -0,0 +1,37 @@
+﻿using FluentValidation;
+using MediatR;
+using SharedKernel.Abstractions.CQRS;
+
+namespace SharedKernel.PipelineBehaviors
+{
+    public class ValidationBehavior<TRequest, TResponse>
+        : IPipelineBehavior<TRequest, TResponse>
+        where TRequest : ICommand<TResponse>
+    {
+        private readonly IEnumerable<IValidator<TRequest>> _validators;
+        public ValidationBehavior(IEnumerable<IValidator<TRequest>> validators)
+        {
+            _validators = validators;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            ValidationContext<TRequest> context = new(request);
+            FluentValidation.Results.ValidationResult[] validationResult =
+                await Task.WhenAll(_validators.Select(x => x.ValidateAsync(context, cancellationToken)));
+
+            List<FluentValidation.Results.ValidationFailure> errors =
+                validationResult
+                .Where(x => x.Errors.Count != 0)
+                .SelectMany(x => x.Errors)
+                .ToList();
+
+            if(errors.Count != 0)
+            {
+                throw new ValidationException(errors);
+            }
+
+            return await next(cancellationToken);
+        }
+    }
+}
