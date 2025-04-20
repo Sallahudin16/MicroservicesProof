86c94d87eec5216d207de5e9629183a083f6d495
Muhammad Salahuddin <mohamed.ayman@assetcom.eg>
2025-04-20 12:30:33 +0200

    Added Get Products Feature

---
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsEndpoint.cs
@@ -0,0 +1,24 @@
+﻿
+namespace Catalog.API.Products.GetProducts
+{
+    //public record GetProductsRequest();
+    public record GetProductResponse(IEnumerable<Product> Products);
+
+    public class GetProductsEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet(RoutingConstants.ProductsRoute, async (ISender sender) =>
+            {
+                GetProductsQuery query = new();
+                GetProductResult result = await sender.Send(query);
+                GetProductResponse response = result.Adapt<GetProductResponse>();
+                return Results.Ok(response);
+            })
+                .WithName("GetProducts")
+                .Produces<GetProductResponse>(StatusCodes.Status200OK)
+                .ProducesProblem(StatusCodes.Status400BadRequest)
+                .WithDescription("Gets all Products");
+        }
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
@@ -0,0 +1,24 @@
+﻿namespace Catalog.API.Products.GetProducts
+{
+    public record GetProductsQuery() : IQuery<GetProductResult>;
+    public record GetProductResult(IEnumerable<Product> Products);
+
+    public class GetProductsHandler : IQueryHandler<GetProductsQuery, GetProductResult>
+    {
+        private readonly IDocumentSession _session;
+        private readonly ILogger<GetProductsHandler> _logger;
+
+        public GetProductsHandler(IDocumentSession session, ILogger<GetProductsHandler> logger)
+        {
+            _session = session;
+            _logger = logger;
+        }
+
+        public async Task<GetProductResult> Handle(GetProductsQuery query, CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Handling GetProductsQuery with: {@Query}", query);
+            IReadOnlyList<Product> products = await _session.Query<Product>().ToListAsync(cancellationToken);
+            return new GetProductResult(products);
+        }
+    }
+}
