07b66eb6291b9651da474fe3cd9b9e2166920ed4
Muhammad Salahuddin <mohamed.ayman@assetcom.eg>
2025-04-20 12:31:03 +0200

    Added Delete Product Feature

---
+++ src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductEndpoint.cs
@@ -0,0 +1,22 @@
+﻿namespace Catalog.API.Products.DeleteProduct
+{
+    //public record DeleteProductRequest(Guid Id)
+    public record DeleteProductResponse(bool IsSuccess);
+    public class DeleteProductEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapDelete(RoutingConstants.ProductsRoute + "/{id:guid}", DeleteProductAsync)
+                .WithName("DeleteProduct")
+                .Produces<DeleteProductResponse>(StatusCodes.Status200OK)
+                .Produces(StatusCodes.Status404NotFound)
+                .WithTags(RoutingConstants.ModuleName);
+        }
+
+        private async Task<IResult> DeleteProductAsync(Guid id, ISender sender)
+        {
+            DeleteProductResult result = await sender.Send(new DeleteProductCommand(id));
+            return Results.Ok(result.Adapt<DeleteProductResponse>());
+        }
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductHandler.cs
@@ -0,0 +1,27 @@
+﻿namespace Catalog.API.Products.DeleteProduct
+{
+    public record DeleteProductCommand(Guid Id) : ICommand<DeleteProductResult>;
+    public record DeleteProductResult(bool IsSuccess);
+
+    internal class DeleteProductHandler : ICommandHandler<DeleteProductCommand, DeleteProductResult>
+    {
+        private readonly IDocumentSession _session;
+        private readonly ILogger<DeleteProductHandler> _logger;
+
+        public DeleteProductHandler(IDocumentSession session, ILogger<DeleteProductHandler> logger)
+        {
+            _session = session;
+            _logger = logger;
+        }
+
+        public async Task<DeleteProductResult> Handle(DeleteProductCommand request, CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Deleting product with id {Id}", request.Id);
+
+            _session.Delete<Product>(request.Id);
+            await _session.SaveChangesAsync(cancellationToken);
+
+            return new DeleteProductResult(true);
+        }
+    }
+}
